3cc5e8cb9a9d84bc0966d3395c1f870a25e35279
Richard Yohanes <richardyohanes15@gmail.com>
2024-09-27 06:56:42 +0700

    fixed animation typewriter

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,4 @@
+
 // Navbar Fixed
 window.onscroll = function () {
     const header = document.querySelector('header');
@@ -21,73 +22,44 @@ hamburger.addEventListener('click', function() {
     navMenu.classList.toggle('hidden')
 })
 
-// animation typewriter
 function sleep(ms) {
-    return new  Promise((resolve) => setTimeout(resolve, ms))
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const phrases = ["Richard Yohanes", "Undergraduate Student at Binus University"]
+const phrases = ["Richard Yohanes", "Student at Binus University"];
 const el = document.getElementById('typewriter');
 
 let sleepTime = 100;
-
-let curPharseIndex = 0;
+let curPhraseIndex = 0;
 
 const writeLoop = async () => {
-    while(true) {
-        let curWord = phrases[curPharseIndex]
+    while (true) {
+        let curWord = phrases[curPhraseIndex];
 
-        for(let i=0; i<phrases.length; i++){
-            el.innerText = curWord.substring(0, i+1);
+        // Ketik kata satu per satu
+        for (let i = 0; i < curWord.length; i++) {
+            el.innerText = curWord.substring(0, i + 1);
             await sleep(sleepTime);
         }
 
-        await sleep(sleepTime * 10);
+        await sleep(sleepTime * 10); // Tunggu setelah mengetik kata
 
-        for(let i = curWord.length; i > 0; i--){
-            el.innerText = curWord.substring(0, i-1);
+        // Hapus kata satu per satu
+        for (let i = curWord.length; i > 0; i--) {
+            el.innerText = curWord.substring(0, i - 1);
             await sleep(sleepTime);
         }
-        
-        await sleep(sleepTime * 5);
 
-        if(curPharseIndex === phrases.length){
-            curPharseIndex = 0;
-        }else {
-            curPharseIndex++;
+        await sleep(sleepTime * 5); // Tunggu sebelum mengetik kata berikutnya
+
+        // Ganti ke kata berikutnya
+        curPhraseIndex++;
+        if (curPhraseIndex >= phrases.length) {
+            curPhraseIndex = 0; // Reset indeks ke awal
         }
     }
-}
+};
 
 writeLoop();
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const words = ['Developer', 'Designer', 'Creator']; // Daftar kata-kata
-//     let i = 0;
 
-//     function typeWriter(word, index, callback) {
-//       if (index < word.length) {
-//         document.getElementById('typewriter').innerHTML = word.substring(0, index + 1);
-//         setTimeout(function () {
-//           typeWriter(word, index + 1, callback);
-//         }, 200); // Kecepatan mengetik
-//       } else {
-//         setTimeout(callback, 1000); // Tunggu sebelum mengganti kata
-//       }
-//     }
-
-//     function startTypewriter() {
-//       if (i < words.length) {
-//         typeWriter(words[i], 0, function () {
-//           i++;
-//           if (i >= words.length) {
-//             i = 0;
-//           }
-//           setTimeout(startTypewriter, 500); // Waktu jeda sebelum kata berikutnya
-//         });
-//       }
-//     }
-
-//     startTypewriter();
-//     }
-// )
