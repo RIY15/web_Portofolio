ea7fb1ec229ddb3779f9184389a054e35004b9ab
Richard Yohanes <richardyohanes15@gmail.com>
2024-09-27 06:31:43 +0700

    modified about and add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
     },
     extend: {
       keyframes: {
+        blink: {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
         bounce: {
           '0%, 100%' : {
             transform: 'translateY(-5%)' ,
@@ -15,11 +19,14 @@ module.exports = {
           '50%': {
             transform: 'translateY(0)'
           }
-        }
+        },
       },
       screen: {
         '2xl': '1320px'
       },
+      animation: {  
+        blink: 'blink 1s linear infinite',
+      }
     },
   },
   plugins: [],
